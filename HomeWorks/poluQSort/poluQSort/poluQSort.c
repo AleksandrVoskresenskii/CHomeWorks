68a774cf0da658305445e9120126340d71b0a11e
AleksandrVoskresenskii <aleksandrvoskresenskii06@gmail.com>
2024-09-30 17:37:53 +0300

    poluQSort C++ --> C

--- HomeWorks/poluQSort/poluQSort/poluQSort.c
+++ HomeWorks/poluQSort/poluQSort/poluQSort.c
@@ -11,9 +11,9 @@ void swap(int* left, int* right) {
     *left ^= *right;
 }
 
-void poluQSort(int* Array, int* lenArray) {
+void poluQSort(int* Array, int lenArray) {
     int point = Array[0];
-    int counterStart = 0, counterEnd = * lenArray - 1;
+    int counterStart = 0, counterEnd = lenArray - 1;
 
     while (counterEnd > counterStart) {
         if (Array[counterStart] < point) {
@@ -31,7 +31,7 @@ void poluQSort(int* Array, int* lenArray) {
 
 bool test1(void) {
     int A[5] = { 3, 1, 5, 5, 2 }, len = 5, B[5] = { 2, 1, 5, 5, 3 };
-    poluQSort(A, &len);
+    poluQSort(A, len);
     return A[0] == B[0] && A[1] == B[1] && A[2] == B[2] && A[3] == B[3] && A[4] == B[4];
 }
 
@@ -45,7 +45,7 @@ int main(void) {
         Array[i] = rand();
     }
     printf("%d\n", Array[0]);
-    poluQSort(Array, &len);
+    poluQSort(Array, len);
     for (int i = 0; i < 30; ++i) {
         printf("%d ", Array[i]);
     }
